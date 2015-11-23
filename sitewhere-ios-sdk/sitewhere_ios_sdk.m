50173b75861a35f4147f5f2750b8f807c8053b1f
Chris Bick <chris@apptop.com>
2015-11-22 20:23:08 -0500

    Initial Commit

---
+++ sitewhere-ios-sdk/sitewhere_ios_sdk.m
@@ -0,0 +1,13 @@
+//
+//  sitewhere_ios_sdk.m
+//  sitewhere-ios-sdk
+//
+//  Created by Chris Bick on 11/22/15.
+//  Copyright © 2015 SiteWhere. All rights reserved.
+//
+
+#import "sitewhere_ios_sdk.h"
+
+@implementation sitewhere_ios_sdk
+
+@end
